49f01c16ae503534a8cfc308171d0e05e0fa8e1a
BadCoder1337 <anton.tiz.musin@gmail.com>
2020-03-19 22:55:06 +0300

    Fetcher refactor

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -8,37 +8,69 @@ export default class Fetcher {
         return raw.map(entry => entry.split(';')[0]).join(';');
     }
 
-    private cookies: string;
-    private weekId: number;
-
-    private async initSession() {
+    static post(url: string, body?: [string, unknown][]) {
         const form = new FormData();
 
-        form.append('login', '');
-        form.append('password', '');
-        form.append('guest', 'Войти как Гость');
+        body.forEach(b => form.append(...b));
 
-        const res = await fetch(
-            'http://scs.sarfti.ru/login/index',
-            {
-                body: form,
-                method: 'POST',
-            }
+        return fetch(
+            url,
+            { body: form, method: 'POST' }
         );
+    }
+
+    constructor(public weekId?: number, public cookies?: string) {}
+
+    // private retries: number;
+
+    private async initSession() {
+        if (this.cookies) { return; }
+
+        const res = await Fetcher.post('http://scs.sarfti.ru/login/index', [
+            ['login', ''],
+            ['password', ''],
+            ['guest', 'Войти как Гость']
+        ]);
 
         this.cookies = Fetcher.parseCookies(res);
     }
 
-    private async fetchSchedule() {
+    private async fetchWeeks() {
+        const res = await Fetcher.post('http://scs.sarfti.ru/date/printT', [
+            ['id', 1],
+            ['show', 'Распечатать']
+        ]);
 
+        return res.text();
     }
 
-    public async getSchedule(week: number) {
+    private async fetchSchedule(week: number) {
+        const res = await Fetcher.post('http://scs.sarfti.ru/date/printT', [
+            ['id', week],
+            ['show', 'Распечатать']
+        ]);
+
+        return res.text();
+    }
+
+    public async getSchedule(week?: number): Promise<string> {
         if (this.cookies) {
-
+            return this.fetchSchedule(week || this.weekId);
         } else {
+            await this.initSession();
 
+            return this.getSchedule();
         }
-
     }
+
+    public async getWeeks(): Promise<string> {
+        if (this.cookies) {
+            return this.fetchWeeks();
+        } else {
+            await this.initSession();
+
+            return this.getWeeks();
+        }
+    }
+
 }
