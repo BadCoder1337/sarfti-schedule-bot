271d06a1f2e074e1c2e7c7cfcdeb1fc840927956
BadCoder1337 <anton.tiz.musin@gmail.com>
2020-03-19 23:57:35 +0300

    DROP_DB env

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,7 +7,7 @@ export default async (url: string, logging = false) => {
     const DB = new Sequelize(url, { logging });
     await DB.authenticate();
     DB.addModels([Person, Lesson, Week]);
-    await DB.sync();
+    await DB.sync({ force: process.env.DROP_DB === 'true' });
 
     return DB;
 };
